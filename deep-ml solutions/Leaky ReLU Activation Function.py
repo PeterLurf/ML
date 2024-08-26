1baaf2cf0a9f9999360553b0a2e6e8973164accc
Peter Lu <peterlujh@gmail.com>
2024-08-25 21:01:28 -0400

    Leaky with tests

--- deep-ml solutions/Leaky ReLU Activation Function.py
+++ deep-ml solutions/Leaky ReLU Activation Function.py
@@ -1,1 +1,17 @@
-asdasd
+import unittest
+
+def leaky_relu(z: float, alpha: float = 0.01) -> float:
+    return z if z >= 0 else alpha * z
+
+class TestLeakyReLU(unittest.TestCase):
+    def test_positive(self):
+        self.assertEqual(leaky_relu(5), 5)
+    
+    def test_negative(self):
+        self.assertEqual(leaky_relu(-5), -0.05)
+    
+    def test_zero(self):
+        self.assertEqual(leaky_relu(0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
